4943b242dea2673f17133664446e3c5967072788
priceymineral <tudaeva@gmail.com>
2022-12-26 14:56:36 -0800

    As a user, when the light switch is "off" I see a white light bulb. As a user, when I turn the switch "on" I see a yellow light bulb.

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ const App = () => {
       <LightSwitch />
       <LightSwitch />
       <LightSwitch />
-      <LightSwitch />
-
     </>
   )
 }
--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react"
 import switchOff from "../assets/switchOff.png"
 import switchOn from "../assets/switchOn.png"
+import lightOn from "../assets/lightOn.png"
+import lightOff from "../assets/lightOff.png"
 
 const LightSwitch = (props) => {
   const [lightSwitchStatus, setLightSwitchStatus] = useState(switchOff)
+  const [lightBulbStatus, setLightBulbStatus] = useState(lightOff)
 
   const handleClick = () => {
     if (lightSwitchStatus === switchOff) {
       setLightSwitchStatus(switchOn)
+      setLightBulbStatus(lightOn)
     } else {
       setLightSwitchStatus(switchOff)
+      setLightBulbStatus(lightOff)
     }
   }
 
@@ -17,11 +22,17 @@ const LightSwitch = (props) => {
     <>
       <div 
       onClick={handleClick} 
-      className="lightbulb"
+      className="lightswitch"
       style={{
         backgroundImage:`url(${lightSwitchStatus})` 
       }}
       >
+      </div>
+      <div
+      className="lightbulb"
+      style={{
+        backgroundImage: `url(${lightBulbStatus})`
+      }}>
       </div>
     </>
   )
